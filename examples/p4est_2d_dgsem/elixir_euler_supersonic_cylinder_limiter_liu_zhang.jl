bbe079faa18736af7f6b8dc912f314aa9e3b19fe
Jesse Chan <jesse.lee.chan@gmail.com>
2026-06-28 13:31:32 -0500

    add general BC

--- examples/p4est_2d_dgsem/elixir_euler_supersonic_cylinder_limiter_liu_zhang.jl
+++ examples/p4est_2d_dgsem/elixir_euler_supersonic_cylinder_limiter_liu_zhang.jl
@@ -32,46 +32,59 @@ equations = CompressibleEulerEquations2D(1.4)
     return prim2cons(prim, equations)
 end
 
+@inline function boundary_condition_outflow_general(u_inner,
+                                                    normal_direction::AbstractVector, x, t,
+                                                    surface_flux_function,
+                                                    equations::CompressibleEulerEquations2D)
+
+    # This would be for the general case where we need to check the magnitude of the local Mach number
+    norm_ = norm(normal_direction)
+    # Normalize the vector without using `normalize` since we need to multiply by the `norm_` later
+    normal = normal_direction / norm_
+
+    # Rotate the internal solution state
+    u_local = Trixi.rotate_to_x(u_inner, normal, equations)
+
+    # Compute the primitive variables
+    rho_local, v_normal, v_tangent, p_local = cons2prim(u_local, equations)
+
+    # Compute local Mach number
+    a_local = sqrt(equations.gamma * p_local / rho_local)
+    Mach_local = abs(v_normal / a_local)
+    if Mach_local <= 1.0 # The `if` is not needed in this elixir but kept for generality
+        # In general, `p_local` need not be available from the initial condition
+        p_local = pressure(initial_condition_subsonic(x, t, equations), equations)
+    end
+
+    # Create the `u_surface` solution state where the local pressure is possibly set from an external value
+    prim = SVector(rho_local, v_normal, v_tangent, p_local)
+    u_boundary = prim2cons(prim, equations)
+    u_surface = Trixi.rotate_from_x(u_boundary, normal, equations)
+
+    # Compute the flux using the appropriate mixture of internal / external solution states
+    return flux(u_surface, normal_direction, equations)
+end
+
 initial_condition = initial_condition_high_mach_flow
-
-# Supersonic inflow boundary condition.
-# Calculate the boundary flux entirely from the external solution state, i.e., set
-# external solution state values for everything entering the domain.
-@inline function boundary_condition_supersonic_inflow(u_inner,
-                                                      normal_direction::AbstractVector,
-                                                      x, t, surface_flux_function,
-                                                      equations::CompressibleEulerEquations2D)
-    u_boundary = initial_condition_high_mach_flow(x, t, equations)
-    return flux(u_boundary, normal_direction, equations)
-end
-
-# Supersonic outflow boundary condition.
-# Calculate the boundary flux entirely from the internal solution state. Analogous to supersonic inflow
-# except all the solution state values are set from the internal solution as everything leaves the domain
-@inline function boundary_condition_outflow(u_inner, normal_direction::AbstractVector, x, t,
-                                            surface_flux_function,
-                                            equations::CompressibleEulerEquations2D)
-    return flux(u_inner, normal_direction, equations)
-end
 
 boundary_conditions = (; Bottom = boundary_condition_slip_wall,
                          Circle = boundary_condition_slip_wall,
                          Top = boundary_condition_slip_wall,
-                         Right = boundary_condition_outflow,
-                         Left = boundary_condition_supersonic_inflow)
+                         Right = boundary_condition_outflow_general,
+                         Left = BoundaryConditionDirichlet(initial_condition_high_mach_flow))
 
 surface_flux = flux_lax_friedrichs
 
 polydeg = 3
 basis = LobattoLegendreBasis(polydeg)
-indicator_ec = IndicatorEntropyCorrection(equations, basis; 
-                                          scaling = 2)
+indicator_ec = IndicatorEntropyCorrection(equations, basis; scaling = 2)
 # indicator_sc = IndicatorHennemannGassner(equations, basis,
-#                                          alpha_max = 0.05,
+#                                          alpha_max = 0.1,
 #                                          alpha_min = 0.00,
 #                                          alpha_smooth = true,
 #                                          variable = density_pressure)
-# indicator = IndicatorEntropyCorrectionShockCapturingCombined(indicator_ec, indicator_sc)
+# indicator_combined = 
+#     IndicatorEntropyCorrectionShockCapturingCombined(indicator_ec, indicator_sc)
 
 volume_integral_default = VolumeIntegralWeakForm()
 volume_integral_entropy_stable = VolumeIntegralPureLGLFiniteVolume(surface_flux)
@@ -103,19 +116,19 @@ ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
 
-alive_callback = AliveCallback(analysis_interval = analysis_interval)
+alive_callback = AliveCallback(alive_interval = 2000)
 
 # The SaveRestartCallback allows to save a file from which a Trixi.jl simulation can be restarted
 save_restart = SaveRestartCallback(interval = 1000,
                                    save_final_restart = true)
 
-save_solution = SaveSolutionCallback(interval = 5000,
+save_solution = SaveSolutionCallback(interval = 1000,
                                      save_initial_solution = true,
                                      save_final_solution = true,
                                      solution_variables = cons2prim)
 
 callbacks = CallbackSet(summary_callback, 
-                        alive_callback,
+                        alive_callback, 
                         save_solution, save_restart)
 
 local_limiter! = PositivityPreservingLimiterZhangShu(thresholds = (1.0e-8, 1.0e-8),
@@ -124,17 +137,22 @@ local_limiter! = PositivityPreservingLimiterZhangShu(thresholds = (1.0e-8, 1.0e-
 global_limiter! = PositivityPreservingLimiterLiuZhang(local_limiter!, semi;
                                                       record_davis_yin_iterations = true)
 
-ode_solver = RDPK3SpFSAL49(; stage_limiter! = global_limiter!,
+ode_solver = RDPK3SpFSAL35(; stage_limiter! = global_limiter!,
                              step_limiter! = global_limiter!)
 
 ###############################################################################
 # run the simulation
 sol = solve(ode, ode_solver;
-            adaptive = true, dt = 1e-7, abstol = 1e-5, reltol = 1e-4,
-            saveat=LinRange(tspan..., 400), callback = callbacks);
+            adaptive = true, dt = 1e-7, abstol = 1e-5, reltol = 1e-3,
+            # adaptive = false, dt = 1,
+            saveat=LinRange(tspan..., 800), callback = callbacks);
 
 # using Plots
 # @gif for i in eachindex(sol.u)
 #     pd = PlotData2D(sol.u[i], semi)
-#     plot(pd["rho"], clims=(0.02, 7.5), title="Time: $(sol.t[i])", dpi=400)
-# end fps=10
+#     plot(pd["rho"], clims=(0.0, 7.0), title="Time: $(sol.t[i])", dpi=400)
+# end 
+
+# using Plots
+# pd = PlotData2D(sol.u[end], semi)
+# plot(pd["rho"], clims=(0.0, 7.0), title="Time: $(sol.t[end])", dpi=400)
